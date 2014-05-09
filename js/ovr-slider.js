cd4c1acc3c428f20d8966e7127ce80adfd5d752d
Luifi <chiappe.luis@gmail.com>
2014-05-09 01:17:49 -0300

    js

--- js/ovr-slider.js
+++ js/ovr-slider.js
@@ -9,32 +9,39 @@
 */
 
 $(document).ready(function(){
-    
+
     var slideW = $('#ovr-slider ul li').width();
     var slides = $('#ovr-slider ul li').size();
-    
+
     /* Set width slider according the number of slides */
     var sliderW = $('#ovr-slider ul').css('width', slideW*slides);
-    
+
     /* Set width slider container iqual to images size */
     $('#ovr-slider').css('width', slideW);
-    
-    $('.arrow-right').click(function(){
-        var li = $('#ovr-slider').find('li');
 
-        $(li).each(function(i){
-            var i = 0;
-            if(i < slides){
-                $(this).animate({
-                marginLeft: -slideW,
-                opacity: 0.4
-            
-                });
-            }
-            
-         });
+    var iterador = 1;
+	var margin = 0;
+
+    $('.arrow-right').click(function(e){
+        e.preventDefault();
+		margin = margin - slideW;
+        $('#ovr-slider ul').animate({
+            marginLeft: margin,
+        }, function() {
+            iterador++
+        });
+    });
         
-    });  
+    $('.arrow-left').click(function(e){
+        e.preventDefault();
+		margin = margin + slideW;
+        $('#ovr-slider ul').animate({
+            marginLeft: margin,
+        }, function() {
+            iterador--
+        });
+    });
+    
    
-	 
+
 });
